efd18def0e0fbbdd0bc05085f245319a60a7afd8
Dani S. <daniela.schomaker@posteo.de>
2025-10-14 13:24:14 +0200

    feat: add level1

--- models/world.class.js
+++ models/world.class.js
@@ -1,17 +1,10 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0), // "0" für die X-Achse, Y-Achse wird über die Klassen gesetzt
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-  ];
+  level = level1;
   canvas;
   ctx;
   keyboard;
-  camera_x = -100; // Kamera soll nach links verschoben werden
+  camera_x = 0; // Kamera soll später verschoben werden
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -20,7 +13,7 @@ class World {
     this.draw();
     this.setWorld(); // "Hilfe", um die Welt an Objekte zu übergeben (Charakter?)
   }
-
+''
   setWorld() {
     this.character.world = this; // mit "this" wird die aktuelle Instanz der World übergeben???
   }
@@ -28,15 +21,15 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // das alte Bild gelöscht
 
-    this.ctx.translate(this.camera_x, 0); // verschiebt die Kamera nach links
+    this.ctx.translate(this.camera_x, 0); // verschiebt die Kamera nach links s.u.
 
-    this.addObjectsToMap(this.backgroundObjects);
+    this.addObjectsToMap(this.level.backgroundObjects);
 
     this.addToMap(this.character); // nur EINER
-    this.addObjectsToMap(this.clouds); // ALLE Objekte eines Arrays
-    this.addObjectsToMap(this.enemies); 
+    this.addObjectsToMap(this.level.clouds); // ALLE Objekte eines Arrays
+    this.addObjectsToMap(this.level.enemies); 
 
-    this.ctx.translate(-this.camera_x, 0); // zurück verschieben ???
+    this.ctx.translate(-this.camera_x, 0); // zurück verschieben ??? s.o.
 
     // Die draw-Methode wird immer wieder aufgerufen (zB 60fps) --> so akzeotieren
     let self = this;
