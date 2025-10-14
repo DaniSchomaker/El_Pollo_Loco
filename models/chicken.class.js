8ed70d2b449c9d9ced6d7c57722aa4afe95c8dd8
Dani S. <daniela.schomaker@posteo.de>
2025-10-14 20:54:20 +0200

    refactor: clean up code

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -19,7 +19,12 @@ class Chicken extends MovableObject {
   }
 
   animate() {
-    this.moveLeft();
+    setInterval(() => {
+      //vordefinierte Funktion "setInterval()"
+      this.moveLeft();
+    }, 1000 / 60); // Alle 1000/60 Millisekunden wird das zwischen {} ausgeführt
+
+    
 
     setInterval(() => {
       // Bilderanmiation
