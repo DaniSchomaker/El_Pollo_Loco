64631ce78bb9571f18c656e491f1656746c7ccef
Dani S. <daniela.schomaker@posteo.de>
2025-10-15 21:06:50 +0200

    feat: add character isDead-animation

--- models/character.class.js
+++ models/character.class.js
@@ -8,7 +8,7 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-23.png",
     "img/2_character_pepe/2_walk/W-24.png",
     "img/2_character_pepe/2_walk/W-25.png",
-    "img/2_character_pepe/2_walk/W-26.png",
+    "img/2_character_pepe/2_walk/W-26.png"
   ];
 
   IMAGES_JUMPING = [
@@ -20,16 +20,27 @@ class Character extends MovableObject {
     "img/2_character_pepe/3_jump/J-36.png",
     "img/2_character_pepe/3_jump/J-37.png",
     "img/2_character_pepe/3_jump/J-38.png",
-    "img/2_character_pepe/3_jump/J-39.png",
+    "img/2_character_pepe/3_jump/J-39.png"
+  ];
+
+  IMAGES_DEAD = [
+    "img/2_character_pepe/5_dead/D-51.png",
+    "img/2_character_pepe/5_dead/D-52.png",
+    "img/2_character_pepe/5_dead/D-53.png",
+    "img/2_character_pepe/5_dead/D-54.png",
+    "img/2_character_pepe/5_dead/D-55.png",
+    "img/2_character_pepe/5_dead/D-56.png",
+    "img/2_character_pepe/5_dead/D-57.png"
   ];
 
   world; // damit wir auf das Keyboard aus der World zugreifen können???
 
-  offset = { // für die Collidion: innerer roter Kasten
+  offset = {
+    // für die Collidion: innerer roter Kasten
     top: 120,
     bottom: 30,
     left: 40,
-    right: 30
+    right: 30,
   };
 
   //////////////////////////////////////////////////////////////////////////////////////
@@ -38,6 +49,7 @@ class Character extends MovableObject {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_JUMPING);
+    this.loadImages(this.IMAGES_DEAD);
     this.applyGravity();
     this.animate();
   }
@@ -68,7 +80,9 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.isAboveGround()) {
+      if (this.isDead()) {
+        this.playAnimation(this.IMAGES_DEAD);
+      } else if (this.isAboveGround()) {
         this.playAnimation(this.IMAGES_JUMPING);
       } else {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
