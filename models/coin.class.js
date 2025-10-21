0b05ecbd2ecf3ff7de1fcf9419705c9962a5e4fb
Dani S. <daniela.schomaker@posteo.de>
2025-10-21 12:06:28 +0200

    feat: add blinking animation to Coin class

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,38 +1,27 @@
-// class Coin extends CollectableObject {
-//   y = 365; // y-Achse aus der Oberklasse wird überschrieben
-//   height = 60;
-//   width = 60;
-
-//   IMAGES = [
-//     "img/8_coin/coin_1.png",
-//     "img/8_coin/coin_2.png"
-//   ];
-
-//   constructor() {
-//     super();
-//     this.loadImages(this.IMAGES);
-//     this.img = this.imageCache[Coin.IMAGES[0]];   
-
-//     this.x = 400 + Math.floor(Math.random() * 10 * 180);
-//     this.y = 100 + Math.floor(Math.random() * 10 * 25);
-//     // this.x = 200 + Math.random() * 500; // Startpunkt: 200px + zufälliger Wert zwischen 0 und 500
-//     // this.speed = 0.15 + Math.random() * 0.5;
-
-//     // this.animate();
-//   }
-// }
-
 class Coin extends CollectableObject {
-  IMAGES = [ 
-    "img/8_coin/coin_1.png",
-    "img/8_coin/coin_2.png"
-  ];
+  IMAGES = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
+
+  offset = {
+    // für die Collidion: innerer roter Kasten
+    top: 50,
+    bottom: 50,
+    left: 35,
+    right: 35,       
+     } ;
 
   constructor() {
     super();
-    this.loadImages(this.IMAGES); 
-    this.img = this.imageCache[this.IMAGES[0]]; // Startframe setzen ???
-    this.x = 400 + Math.floor(Math.random() * 10 * 180);
-    this.y = 100 + Math.floor(Math.random() * 10 * 20);
+    this.loadImages(this.IMAGES);
+    this.img = this.imageCache[this.IMAGES[0]];
+    this.x = 400 + Math.floor(Math.random() * 1800);
+    this.y = 100 + Math.floor(Math.random() * 200);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES);
+    }, 700);
   }
 }
