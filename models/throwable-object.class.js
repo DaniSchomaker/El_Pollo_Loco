24d8f6475ab091bbdb83d32c8df863d2ea92e592
Dani S. <daniela.schomaker@posteo.de>
2025-10-27 10:20:16 +0100

    feat: add IMAGES_BOTTLE_ROTATION and SPLASH

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -2,7 +2,25 @@ class ThrowableObject extends MovableObject {
   // speedY = 30;
   // speedX = 20;
 
-  hasHit = false;
+  speedX = 10; // Standard: nach rechts
+
+  // hasHit = false;
+
+  IMAGES_BOTTLE_ROTATION = [
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  IMAGES_BOTTLE_SPLASH = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+  ];
 
   constructor(x, y) {
     super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
@@ -17,17 +35,19 @@ class ThrowableObject extends MovableObject {
     this.speedY = 30;
     this.applyGravity();
     setInterval(() => {
-        this.x += 10;
+        // this.x += 10;
+        this.x += this.speedX; // nutzt Vorzeichen für Links/Rechts
     }, 25);
 
   }
 
-    markHit() {
-    this.hasHit = true;
-    this.speed = 0;
-    this.speedY = 0;
-    this.acceleration = 0;
-    // Optional: Splash anzeigen oder später entfernen
-    // this.playAnimation(this.IMAGES_SPLASH);
-  }
+  //   markHit() {
+  //   this.hasHit = true;
+  //   this.speed = 0;
+  //   this.speedY = 0;
+  //   this.acceleration = 0;
+  //   // Optional: Splash anzeigen oder später entfernen
+  //   // this.playAnimation(this.IMAGES_SPLASH);
+  // }
 }
+
