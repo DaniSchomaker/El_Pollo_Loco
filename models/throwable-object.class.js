33d2a34b5f199137d4222788db32c3286267ad07
Dani S. <daniela.schomaker@posteo.de>
2025-10-28 12:54:14 +0100

    feat: add bottle rotation animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -4,13 +4,13 @@ class ThrowableObject extends MovableObject {
 
   speedX = 10; // Standard: nach rechts
 
-  // hasHit = false;
+  hasHit = false;
 
   IMAGES_BOTTLE_ROTATION = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png"
   ];
 
   IMAGES_BOTTLE_SPLASH = [
@@ -23,7 +23,9 @@ class ThrowableObject extends MovableObject {
   ];
 
   constructor(x, y) {
-    super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
+    super().loadImage("img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png");
+    this.loadImages(this.IMAGES_BOTTLE_ROTATION);
+    this.loadImages(this.IMAGES_BOTTLE_SPLASH);
     this.x = x;
     this.y = y;
     this.height = 60;
@@ -34,20 +36,29 @@ class ThrowableObject extends MovableObject {
   throw() {
     this.speedY = 30;
     this.applyGravity();
+
+    // Bewegung (horizontal)
     setInterval(() => {
-        // this.x += 10;
-        this.x += this.speedX; // nutzt Vorzeichen für Links/Rechts
+      if (this.hasHit) return; // nach Treffer: Bewegung stoppen
+      this.x += this.speedX;
     }, 25);
 
+    // Flug-Animation (Rotation)
+    setInterval(() => {
+      if (this.hasHit) return; // nach Treffer: keine Animation mehr
+      this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
+    }, 80); 
+
+    if (this.hasHit) {console.log("Bottle has hit");
+    }
   }
 
-  //   markHit() {
-  //   this.hasHit = true;
-  //   this.speed = 0;
-  //   this.speedY = 0;
-  //   this.acceleration = 0;
-  //   // Optional: Splash anzeigen oder später entfernen
-  //   // this.playAnimation(this.IMAGES_SPLASH);
-  // }
+  markHit() {
+    this.hasHit = true;
+    this.speed = 0;
+    this.speedY = 0;
+    this.acceleration = 0;
+    
+    //this.playAnimation(this.IMAGES_SPLASH);
+  }
 }
-
