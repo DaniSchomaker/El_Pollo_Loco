c34734e6678ec11d59ad884258d6b00fc8338973
Dani S. <daniela.schomaker@posteo.de>
2025-10-31 11:39:47 +0100

    feat: add showEndscreen(type)

--- js/game.js
+++ js/game.js
@@ -2,6 +2,9 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
+let intervalIDs = [];
+let i = 1;
+
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
@@ -61,3 +64,34 @@ window.addEventListener("keyup", (e) => {
     keyboard.D = false;
   }
 });
+
+// function startGame() {
+    
+    // document.getElementById('end_screen_winner').classList.add('d_none');
+    // document.getElementById('gameOverScreen').classList.add('d_none');
+    // document.getElementById('startScreenContainer').classList.add('d_none');
+    // document.getElementById('hud').classList.remove('d_none');
+    // document.getElementById('canvas').style.display = "block";
+
+    // init();
+// }
+
+function showEndscreen(type) {
+  const screen = document.getElementById('endscreen');
+  const img = document.getElementById('endscreen_img');
+
+  // Auswahl je nach Zustand:
+  if (type === 'win') {
+    img.src = '../img/9_intro_outro_screens/YouWon.png';
+  } else {
+    img.src = '../img/9_intro_outro_screens/game_over/oh no you lost!.png';
+  }
+
+  screen.classList.remove('d_none');
+
+  SoundHub.pauseAll(); 
+}
+
+function startGame() {
+  location.reload();  
+}
