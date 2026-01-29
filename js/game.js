ad52c3965d640c22414c2d0c782b70f0aaae16b4
Dani S. <daniela.schomaker@posteo.de>
2026-01-29 14:55:41 +0100

    feat: add start screen

--- js/game.js
+++ js/game.js
@@ -5,6 +5,38 @@ let keyboard = new Keyboard();
 let intervalIds = [];
 let i = 1; // brauche ich das?
 
+let gameStarted = false;
+
+function startGame() {
+  if (gameStarted) {
+    return;
+  }
+
+  gameStarted = true;
+  hideStartScreen();
+  init();
+}
+
+function hideStartScreen() {
+  const startScreen = document.getElementById("start_screen");
+  startScreen.classList.add("d_none");
+}
+
+function openControls() {
+  const overlay = document.getElementById("controls_overlay");
+  overlay.classList.remove("d_none");
+}
+
+function closeControls() {
+  const overlay = document.getElementById("controls_overlay");
+  overlay.classList.add("d_none");
+}
+
+
+function stopClick(event) {
+  event.stopPropagation();
+}
+
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
@@ -41,7 +73,7 @@ window.addEventListener("keyup", (e) => {
   // Wenn Taste LOSGELASSEN wird
   if (e.key === " ") {
     keyboard.SPACE = false;
-    e.preventDefault();   // Browser soll NICHT seinen eigenen Play/Pause-Quatsch machen
+    e.preventDefault(); // Browser soll NICHT seinen eigenen Play/Pause-Quatsch machen
   }
 
   if (e.key === "ArrowLeft") {
@@ -66,34 +98,34 @@ window.addEventListener("keyup", (e) => {
 });
 
 // function startGame() {
-    
-    // document.getElementById('end_screen_winner').classList.add('d_none');
-    // document.getElementById('gameOverScreen').classList.add('d_none');
-    // document.getElementById('startScreenContainer').classList.add('d_none');
-    // document.getElementById('hud').classList.remove('d_none');
-    // document.getElementById('canvas').style.display = "block";
 
-    // init();
+// document.getElementById('end_screen_winner').classList.add('d_none');
+// document.getElementById('gameOverScreen').classList.add('d_none');
+// document.getElementById('startScreenContainer').classList.add('d_none');
+// document.getElementById('hud').classList.remove('d_none');
+// document.getElementById('canvas').style.display = "block";
+
+// init();
 // }
 
 function showEndscreen(type) {
-  const screen = document.getElementById('endscreen');
-  const img = document.getElementById('endscreen_img');
+  const screen = document.getElementById("endscreen");
+  const img = document.getElementById("endscreen_img");
 
   // Auswahl je nach Zustand:
-  if (type === 'win') {
-    img.src = '../img/9_intro_outro_screens/YouWon.png';
+  if (type === "win") {
+    img.src = "../img/9_intro_outro_screens/YouWon.png";
   } else {
-    img.src = '../img/9_intro_outro_screens/game_over/oh no you lost!.png';
+    img.src = "../img/9_intro_outro_screens/game_over/oh no you lost!.png";
   }
 
-  screen.classList.remove('d_none');
+  screen.classList.remove("d_none");
 
-  SoundHub.pauseAll(); 
+  SoundHub.pauseAll();
 }
 
-function startGame() {
-  location.reload();  
+function restartGame() {
+  location.reload();
 }
 
 function setStoppableInterval(fn, time) {
